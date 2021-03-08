c2378dfd1254185958c0740c49aaeda69c0743d5
SeJoMaCode <sethmanche02@gmail.com>
2021-03-08 13:20:16 -0600

    added comments

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 let c;
 let ctx;
 
+// initialize everything when page loads
 window.onload = init;
 
+//what to initialize
 function init(){
     c = document.querySelector('canvas')
     ctx = c.getContext('2d')
     resize()
 
+    // start the game loop
     window.requestAnimationFrame(gameLoop)
 }
 
+//game loop
 function gameLoop(timeStamp){
     window.onresize = resize()
     draw()
     window.requestAnimationFrame(gameLoop)
 }
 
+//resize canvas to the screen
 function resize(){
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 }
 
+//class for the ball object
 class Ball{
+    //set the initial properties of the ball
     constructor(x, y){
         this.pos = [x,y]
         this.velocity = [0,0]
@@ -31,15 +38,19 @@ class Ball{
         this.termV = 10
     }
 
+    //add force the the ball
     AddForce(x, y){
         this.velocity[0] += x
         this.velocity[1] += y
     }
 
+    //logic for moving ball
     move(){
+        //add velocity to position
         this.pos[0] +=  this.velocity[0]
         this.pos[1] +=  this.velocity[1]
 
+        //bounce off top and bottom and lose some velocity
         if(!(this.radius < this.pos[1])){
             this.pos[1] = this.radius
             this.velocity[1] *= -1*1/2
@@ -47,7 +58,8 @@ class Ball{
             this.pos[1] = c.height-this.radius
             this.velocity[1] *= -1*1/2
         }
-        
+
+        //bounce off left and right and lose some velocity
         if(!(this.radius < this.pos[0])){
             this.pos[0] = this.radius
             this.velocity[0] *= -1*7/10
@@ -56,10 +68,11 @@ class Ball{
             this.velocity[0] *= -1*7/10
         }
 
+        //apply gravity if in the air
         if (this.pos[1] < c.height-this.radius){
             this.velocity[1] += this.gravity
         } else {
-            this.pos[1] = c.height-this.radius
+            //lose speed from rolling on the ground
             if (this.velocity[0] < .5 && this.velocity[0] > -.5){
                 this.velocity[0] = 0
             } else if (this.velocity[0] > 0){
@@ -70,6 +83,7 @@ class Ball{
         }
     }
 
+    //draw the ball
     draw(){
         ctx.fillStyle = '#000'
         ctx.beginPath()
@@ -78,8 +92,10 @@ class Ball{
     }
 }
 
+//make the instance of the ball
 const ball = new Ball(500,100)
 
+//listen for keypresses and add force
 document.addEventListener('keydown', e => {
     if(e.key=='w' && ball.pos[1] > c.height-(ball.radius+5)){
         ball.AddForce(0, -20)
@@ -92,8 +108,8 @@ document.addEventListener('keydown', e => {
     }
 })
 
+//draw and move the ball in the game loop
 function draw(){
-    // console.log(ball.pos[1])
     ball.move()
     ball.draw()
 }
